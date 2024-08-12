9eaf9d7ae69c56de11aa6ab7a222c657a53ceee6
Aziz <141174883+AzizDevCraft@users.noreply.github.com>
2024-08-12 11:50:24 +0200

    tout les fonction que je dois coder

---
+++ main.py
@@ -0,0 +1,54 @@
+class DictionnaireOrdonne : 
+    
+    def __init__ (self, *individual, **pret) : 
+        self.keys = []
+        self.value = []
+        
+    
+    def __repr__ (self) : 
+        pass
+    
+    def __str__ (self) : 
+        pass
+    
+    def keys (self) : 
+        pass
+    
+    def values (self) : 
+        pass 
+    
+    def items (self) : 
+        pass 
+    
+    def __len__ (self) : 
+        pass
+    
+    def __add__ (self, ) : 
+        pass
+    
+    def __radd__ (self, ) : 
+        pass
+    
+    def __contains__ (self, ) : 
+        pass 
+    
+    def __getitem__ (self, ) :
+        pass 
+    
+    def __setitem__ (self, ) :
+        pass 
+    
+    def __del__ (self, ) :
+        pass 
+    
+    def __delitem__ (self, ) : 
+        pass
+    
+    def reverse (self, ) : 
+        pass
+    
+    def sort (self, ) :
+        pass
+    
+    def __iter__ (self, ) :
+        pass 
